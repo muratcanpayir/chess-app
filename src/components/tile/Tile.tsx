2daac38925fe0c005f449ae6d75a15c92ae1ebc4
Muratcan Payir <muratcanpayir@gmail.com>
2022-01-11 11:02:16 +0000

    pieces added to chessboard

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 import "./Tile.css";
 
-interface Props{
-  number:number
+interface Props {
+  image?: string;
+  number: number;
 }
 
-function Tile({number}:Props) {
-  if(number%2===0){
-    return <div className='tile dark-tile'></div>
+function Tile({ number, image }: Props) {
+  if (number % 2 === 0) {
+    return (
+      <div className="tile dark-tile">
+        <img src={image} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="tile light-tile">
+        <img src={image} />
+      </div>
+    );
   }
-  else{
-    return <div className='tile light-tile'></div>
-  }
-
 }
 
-export default Tile
+export default Tile;
