c18c143e70b164f22396a74d5f2c59b0689b3106
Payir <muratcanpayir@gmail.com>
2022-01-18 16:14:57 +0300

    created pawn logic

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,8 +1,19 @@
 import { PieceType, TeamType } from "../components/chessboard/Chessboard"
 
 export default class Referee {
-    isValidMove(px: number, py: number, x: number, y: number, type: PieceType,team:TeamType) {
-        console.log("Referee is checking the move...");
-        return true;
+    isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType) {
+
+        if (type === PieceType.PAWN) {
+            if (team === TeamType.OUR) {
+                if (py === 1) {
+                    if (px === x && (y - py === 1 || y - py === 2)) {
+                        console.log("valid move");
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
