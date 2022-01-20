88962ca4e92f48424d6b6ca08bf3f8d29db44f82
Payir <muratcanpayir@gmail.com>
2022-01-20 11:52:51 +0300

    white pawns occupied moves fixed

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,18 +1,31 @@
-import { PieceType, TeamType } from "../components/chessboard/Chessboard"
+import { PieceType, TeamType, Piece } from "../components/chessboard/Chessboard"
 
 export default class Referee {
-    isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType) {
+
+    tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+        const piece = boardState.find((p) => p.x === x && p.y === y);
+        if (piece) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType, boardState: Piece[]) {
 
         if (type === PieceType.PAWN) {
             if (team === TeamType.OUR) {
                 if (py === 1) {
                     if (px === x && (y - py === 1 || y - py === 2)) {
-                        console.log("valid move");
-                        return true;
+                        if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
                     }
                 } else {
                     if (px === x && y - py === 1) {
-                        return true;
+                        if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
                     }
                 }
             } else {
